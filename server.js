ac826223ff78cf38d35a36fc1ee6c85a92063d25
Abdurahim-Yoldoshev <yoldoshevcoder@gmail.com>
2025-12-24 11:32:41 +0500

    del

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-app.use(express.static('public'));
-app.listen(port, () => {
-    console.log(`Game is running on port ${port}`);
-    console.log(`Open your browser and go to http://localhost:${port}`);
-});
